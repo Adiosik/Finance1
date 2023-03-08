7f159ac6b3b8ca341a4257180a466d7409f3f69d
Daniel <simansky.dan@gmail.com>
2023-03-08 19:07:27 +0100

    New project

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -1,7 +1,21 @@
+import java.io.BufferedInputStream;
+import java.net.URL;
 
 class Main {
-    public static void main(String[] args) {
-        System.out.println("hello world");
+    private static String APIkey = "V2M8GQGGP5LUC19";
+
+    public static void main(String[] args) throws Exception {
+        getCompany("AMD");
     }
+
+    public static void getCompany(String key) throws Exception {
+
+        String urlValue = "https://www.alphavantage.co/query?function=SYMBOL_SEARCH&keywords=" + key + "&apikey=" + APIkey;
+        URL url = new URL(urlValue);
+        BufferedInputStream stream = new BufferedInputStream(url.openConnection().getInputStream());
+        String result = new String(stream.readAllBytes());
+        System.out.println(result);
+    }
+
 }
 
